e65ba3ce311f828162ec046f1d2ec9b06ff05fde
RobsonDev <robson.arcoleze29@gmail.com>
2023-01-21 15:31:44 -0300

    Created Project

---
+++ backend/src/test/java/br/com/robsonarcoleze/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.robsonarcoleze.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
