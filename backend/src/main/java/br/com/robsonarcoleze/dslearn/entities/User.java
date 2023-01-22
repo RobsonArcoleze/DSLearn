5cb1ff1cc7017d17dc3f734764ddc5ec875e0ede
RobsonDev <robson.arcoleze29@gmail.com>
2023-01-21 22:15:11 -0300

    Created entities, seeding database, ORM

---
+++ backend/src/main/java/br/com/robsonarcoleze/dslearn/entities/User.java
@@ -0,0 +1,41 @@
+package br.com.robsonarcoleze.dslearn.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter private Long id;
+	@Getter @Setter private String name;
+	@Getter @Setter private String email;
+	@Getter @Setter private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role",
+	joinColumns = @JoinColumn(name = "user_id"),
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
+	@Getter private Set<Role> roles = new HashSet<>();
+}
