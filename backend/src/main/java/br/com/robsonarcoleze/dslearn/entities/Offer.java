5539c06bfb7d654a13b842e0ad95b27642949f5b
RobsonDev <robson.arcoleze29@gmail.com>
2023-01-24 13:44:27 -0300

    Implementado Offer, Feito ORM OneToMany, ManyToOne

--- backend/src/main/java/br/com/robsonarcoleze/dslearn/entities/Offer.java
+++ backend/src/main/java/br/com/robsonarcoleze/dslearn/entities/Offer.java
@@ -1,14 +1,14 @@
 package br.com.robsonarcoleze.dslearn.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -16,22 +16,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 @Entity
-@Table(name = "tb_course")
-public class Course implements Serializable{
+@Table(name = "tb_offer")
+public class Offer implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Getter @Setter private Long id;
-	@Getter @Setter private String name;
-	@Getter @Setter private String imgUri;
-	@Getter @Setter private String imgGrayUri;
+	@Getter @Setter private String edition;
+	@Getter @Setter private Instant startMoment;
+	@Getter @Setter private Instant endMoment;
 	
-	@OneToMany(mappedBy = "course")
-	@Getter private List<Offer> offers = new ArrayList<>();
+	@ManyToOne
+	@JoinColumn(name = "course_id")
+	@Getter @Setter private Course course;
 }
